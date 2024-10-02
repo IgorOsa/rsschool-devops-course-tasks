f34752249bc15a0069486cc675a99f21bba4f4e0
Ihor <igorosa.com@gmail.com>
2024-10-02 21:48:32 +0300

    Update tfstate resources

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
-
 module "iam" {
   source         = "./modules/iam"
   tags           = var.tags
   aws_account_id = var.aws_account_id
+}
+
+module "tfstates" {
+  source = "./modules/tf-states"
+  tags   = var.tags
 }
