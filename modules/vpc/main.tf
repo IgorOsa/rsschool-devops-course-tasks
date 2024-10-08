d8722a3ee201967ac633050dabf868453a33870f
Ihor <igorosa.com@gmail.com>
2024-10-08 10:50:08 +0300

    Update vpc with subnets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,21 +5,69 @@ resource "aws_vpc" "vpc" {
 resource "aws_subnet" "public_subnet_1" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = var.public_subnet_cidr_blocks[0]
-  availability_zone = "eu-central-1a"
-
-  tags = {
-    Name = "Main"
-  }
+  availability_zone = var.availability_zones[0]
 }
 
 resource "aws_subnet" "public_subnet_2" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = var.public_subnet_cidr_blocks[1]
-  availability_zone = "eu-central-1b"
+  availability_zone = var.availability_zones[1]
+}
 
-  tags = {}
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.private_subnet_cidr_blocks[0]
+  availability_zone = var.availability_zones[0]
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.private_subnet_cidr_blocks[1]
+  availability_zone = var.availability_zones[1]
 }
 
 resource "aws_internet_gateway" "internet_gateway" {
   vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+}
+
+resource "aws_route_table" "default_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_1_association" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_subnet_2_association" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route_table_association" "private_subnet_1_association" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_subnet_2_association" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_route_table.id
 }
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -10,6 +10,18 @@ variable "public_subnet_cidr_blocks" {
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
+variable "private_subnet_cidr_blocks" {
+  description = "CIDR blocks for the private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["eu-central-1a", "eu-central-1b"]
+}
+
 variable "tags" {
   type    = map(string)
   default = {}
