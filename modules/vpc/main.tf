2bdc116373da8edae6f77020bf58ebc6d7793f85
Ihor <igorosa.com@gmail.com>
2024-10-08 18:53:55 +0300

    Add security group

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,41 +1,56 @@
 resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr_block
+
+  tags = {
+    Name = "VPC"
+  }
 }
 
 resource "aws_subnet" "public_subnet_1" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = var.public_subnet_cidr_blocks[0]
   availability_zone = var.availability_zones[0]
+
+  tags = {
+    Name = "Public Subnet 1"
+  }
 }
 
 resource "aws_subnet" "public_subnet_2" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = var.public_subnet_cidr_blocks[1]
   availability_zone = var.availability_zones[1]
+
+  tags = {
+    Name = "Public Subnet 2"
+  }
 }
 
 resource "aws_subnet" "private_subnet_1" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = var.private_subnet_cidr_blocks[0]
   availability_zone = var.availability_zones[0]
+
+  tags = {
+    Name = "Private Subnet 1"
+  }
 }
 
 resource "aws_subnet" "private_subnet_2" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = var.private_subnet_cidr_blocks[1]
   availability_zone = var.availability_zones[1]
+
+  tags = {
+    Name = "Private Subnet 2"
+  }
 }
 
 resource "aws_internet_gateway" "internet_gateway" {
   vpc_id = aws_vpc.vpc.id
-}
 
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.internet_gateway.id
+  tags = {
+    Name = "Internet GW"
   }
 }
 
@@ -46,20 +61,28 @@ resource "aws_route_table" "default_route_table" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.internet_gateway.id
   }
+
+  tags = {
+    Name = "Public route table"
+  }
 }
 
 resource "aws_route_table_association" "public_subnet_1_association" {
   subnet_id      = aws_subnet.public_subnet_1.id
-  route_table_id = aws_route_table.public_route_table.id
+  route_table_id = aws_route_table.default_route_table.id
 }
 
 resource "aws_route_table_association" "public_subnet_2_association" {
   subnet_id      = aws_subnet.public_subnet_2.id
-  route_table_id = aws_route_table.public_route_table.id
+  route_table_id = aws_route_table.default_route_table.id
 }
 
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "Private route table"
+  }
 }
 
 resource "aws_route_table_association" "private_subnet_1_association" {
@@ -71,3 +94,29 @@ resource "aws_route_table_association" "private_subnet_2_association" {
   subnet_id      = aws_subnet.private_subnet_2.id
   route_table_id = aws_route_table.private_route_table.id
 }
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound and outbound traffic"
+
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Allow all traffic SG"
+  }
+}
+
